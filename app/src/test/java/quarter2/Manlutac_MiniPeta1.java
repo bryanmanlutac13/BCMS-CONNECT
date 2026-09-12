6a5dfab596639805ce416b26c02c5871c1130f76
Bryan <bryanmanlutac13@gmail.com>
2026-09-12 23:12:58 +0800

    Added Manlutac_MiniPeta1.java

---
+++ app/src/test/java/quarter2/Manlutac_MiniPeta1.java
@@ -0,0 +1,18 @@
+package quarter2;
+
+import org.junit.Test;
+
+public class Manlutac_MiniPeta1 {
+    @Test
+    public void home() {
+        String firstName = "Bryan";
+        String middleName = "D.";
+        String lastName = "Manlutac";
+        int age = 18;
+        String petName = "Jupyter";
+        String favFood = "Bicol express";
+        System.out.println("===========================================================================================================");
+        System.out.println("My name is " + firstName + " " + middleName + " " + lastName + ". " + "Im " + age + "years old" + ", my pet name is " + petName + " and " + favFood + " is my favorite food.");
+        System.out.println("===========================================================================================================");
+    }
+}
