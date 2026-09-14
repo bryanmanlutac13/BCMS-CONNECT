0b2683700958bb13ddb1f39927a4bb93a63114f8
princejoelmirande-design <princejoel.mirande@gmail.com>
2026-09-14 13:39:52 +0800

    added Mirande_MiniPeta1

--- app/src/test/java/quarter2/Mirande_MiniPeta1.java
+++ app/src/test/java/quarter2/Mirande_MiniPeta1.java
@@ -2,21 +2,21 @@ package quarter2;
 
 import org.junit.Test;
 
-public class Caringal_MiniPeta1 {
+public class Mirande_MiniPeta1 {
 
     @Test
     public void testProfile() {
 
         // 1. THE INPUT (Storing your personal details in variables)
-        String myName = "Alzhen";
-        String petName = "Snow";
-        String favFood = "BurgerSteak";
-        int myAge = 17;
+        String myName = "Mirande";
+        String petName = "Zargon";
+        String favFood = "Adobo";
+        int myAge = 16;
 
         // 2. THE OUTPUT (Printing to the console)
 //        System.out.println("-TIILE ---");
         System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
-        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("I have a pet named " + petName + ".");
         System.out.println("If I could, I would eat " + favFood + " every single day!");
     }
 }
