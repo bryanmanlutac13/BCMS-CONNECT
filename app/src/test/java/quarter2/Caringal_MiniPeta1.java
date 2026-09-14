865e8e886619224a1c3ef7284a61f635aa023841
bryanmanlutac13 <bryanmanlutac13@gmail.com>
2026-09-14 13:27:15 +0800

    FIXING CODE

--- app/src/test/java/quarter2/Caringal_MiniPeta1.java
+++ app/src/test/java/quarter2/Caringal_MiniPeta1.java
@@ -14,9 +14,11 @@ public class Caringal_MiniPeta1 {
         int myAge = 17;
 
         // 2. THE OUTPUT (Printing to the console)
+        System.out.println("===================================================");
         System.out.println("--- MY DIGITAL PROFILE ---");
         System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
         System.out.println("I have a wonderful pet named " + petName + ".");
         System.out.println("If I could, I would eat " + favFood + " every single day!");
+        System.out.println("===================================================");
     }
 }
