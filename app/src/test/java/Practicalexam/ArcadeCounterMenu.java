86e1df62dae77e2cf79117aa81a7a8a3777908c1
princejoelmirande-design <princejoel.mirande@gmail.com>
2026-09-27 02:03:16 +0800

    Added loop practical exam

--- app/src/test/java/Practicalexam/ArcadeCounterMenu.java
+++ app/src/test/java/Practicalexam/ArcadeCounterMenu.java
@@ -7,5 +7,16 @@ public class ArcadeCounterMenu {
     public void start(Scanner scanner) {
 
         boolean running = true;
+
+        while (running) {
+
+            System.out.println("\n==============================");
+            System.out.println("       ARCADE COUNTER");
+            System.out.println("==============================");
+            System.out.println("1. Buy Tokens");
+            System.out.println("2. Claim Prize");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+        }
     }
 }
